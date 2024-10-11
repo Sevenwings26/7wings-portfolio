850c72994681ca677ccf9a72ba64f110b9d8019d
Sevenwings26 <iarowosola@yahoo.com>
2024-10-11 12:14:14 +0100

    database

--- project/settings.py
+++ project/settings.py
@@ -94,12 +94,12 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
 
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio_db',
-        'USER': 'postgres',
-        'PASSWORD': "iarowosola#%4956i",
-        'HOST': 'localhost',
-        'PORT': '5432'
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'portfolio_db',
+        # 'USER': 'postgres',
+        # 'PASSWORD': "iarowosola#%4956i",
+        # 'HOST': 'localhost',
+        # 'PORT': '5432'
     }
 }
 
