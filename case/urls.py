1d4b8d41f1e4d9bbafa24ed71d634a099e9a38ff
Iyanu Arowosola <iarowosola@yahoo.com>
2025-06-07 20:32:08 +0100

    social-icon update

--- case/urls.py
+++ case/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from .import views
 
-
 urlpatterns = [
-    path('new/', views.index, name="index"),
-    # path('contact/', views.contact_form, name="contact"),
-    # path('service-detail/<int:service_id>/', views.service_detail, name="service_details"),
-    # path('about/', views.about, name="about"),
-    # path('resume/', views.resume, name="resume"),
-    # path('portfolio/', views.portfolio, name="portfolio"),
+    path('', views.index, name="index"),
 ]
 
 
